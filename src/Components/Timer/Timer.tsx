375b039f8cc6c856f24b80e4163511fb41b51909
EhsanShakil <ahsanshakeel1994@gmail.com>
2020-09-10 22:33:55 +0500

    Update Timer.tsx

--- src/Components/Timer/Timer.tsx
+++ src/Components/Timer/Timer.tsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Timer.css";
 
 const Timer = () => {
+  const [minutes, setMinutes] = useState<number>(25);
+  const [seconds, setSeconds] = useState<number>(0);
+  const [isStart, setIsStart] = useState<boolean>(false);
+
   return (
     <div>
-      <h1>Timer Component</h1>
+      <h1>
+        {minutes}:{seconds < 10 ? `0${seconds}` : seconds}
+      </h1>
     </div>
   );
 };
