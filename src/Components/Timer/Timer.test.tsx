133e290ade57980cbd76c59f1f47e3f9b67ab537
EhsanShakil <ahsanshakeel1994@gmail.com>
2020-09-17 22:23:22 +0500

    creating timerbutton test case

--- src/Components/Timer/Timer.test.tsx
+++ src/Components/Timer/Timer.test.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { shallow, configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import Timer from "./Timer";
-import TimerButton from "../TimerButton/TimerButton";
 
 configure({ adapter: new Adapter() });
 let container = shallow(<Timer />);
--- src/Components/TimerButton/TimerButton.test.tsx
+++ src/Components/TimerButton/TimerButton.test.tsx
@@ -1,1 +1,14 @@
 import React from "react";
+import { shallow, configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import TimerButton from "./TimerButton";
+
+configure({ adapter: new Adapter() });
+
+let container = shallow(<TimerButton />);
+
+describe("Timer Button", () => {
+  it("Should render a <div>", () => {
+    expect(container.find("div").length).toEqual(1);
+  });
+});
