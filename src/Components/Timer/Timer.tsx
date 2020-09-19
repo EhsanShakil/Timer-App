b31bee6c9390ee008b4be53652e8b57db5d60d47
EhsanShakil <ahsanshakeel1994@gmail.com>
2020-09-19 16:31:43 +0500

    adding all functionality

--- src/Components/Timer/Timer.tsx
+++ src/Components/Timer/Timer.tsx
@@ -6,40 +6,55 @@ const Timer = () => {
   let [minutes, setMinutes] = useState<number>(0);
   let [seconds, setSeconds] = useState<number>(0);
   let [isStart, setIsStart] = useState<boolean>(false);
-  let [number, setNumber] = useState<number>(0);
+  let [myInterval, setMyInterval] = useState<any>(0);
+
+  let interval: number = 1000;
 
   const Start = () => {
     if (isStart === true) {
       return;
     }
-    let myInterval = setInterval(() => {
-      if (seconds >= 0) {
-        setSeconds(++seconds);
 
-        if (seconds === 60) {
-          setMinutes(++minutes);
-          seconds = 0;
+    setMyInterval(
+      setInterval(() => {
+        if (seconds >= 0) {
+          setSeconds(++seconds);
+
+          if (seconds === 60) {
+            setMinutes(++minutes);
+            seconds = 0;
+          }
         }
-      }
-    }, 1000);
-    setIsStart(true);
+      }, interval)
+    );
+
     console.log("Start");
+    setIsStart(true);
   };
   const Stop = () => {
-    // clearInterval();
-    // setIsStart(false);
+    clearInterval(myInterval);
+
+    setIsStart(false);
     console.log("Stop");
   };
 
   const Reset = () => {
-    console.log("Reset");
+    Stop();
+    setMinutes(0);
+    setSeconds(0);
   };
   return (
     <div className="container">
       <div className="display">
-        <div className="minutes">{minutes < 10 ? `0${minutes}` : minutes}</div>
+        <div className="minutes">
+          <p className="text">Minutes</p>
+          {minutes < 10 ? `0${minutes}` : minutes}
+        </div>
         <div className="collon">:</div>
-        <div className="seconds">{seconds < 10 ? `0${seconds}` : seconds}</div>
+        <div className="seconds">
+          <p className="text">Seconds</p>
+          {seconds < 10 ? `0${seconds}` : seconds}
+        </div>
       </div>
       <div className="buttons">
         <TimerButton buttonAction={Start} buttonValue={"Start"} />
